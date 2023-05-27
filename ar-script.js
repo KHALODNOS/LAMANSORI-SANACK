b08b8aa8f3f23005ff968f2a6647ace4e9f74c21
Khalid Moumine <126973430+KHALODNOS@users.noreply.github.com>
2023-05-27 23:52:44 +0100

    Add files via upload

---
+++ ar-script.js
@@ -0,0 +1,30 @@
+let navbar = document.querySelector(' .header .navbar')
+let menuBtn = document.querySelector('#menu-btn')
+
+menuBtn.onclick = ()  => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    searchBtn.classList.remove('fa-times');
+}
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove("active");
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove("active");
+
+    if (Window.scrollY > 0) {
+        document.querySelector(".header").classList.add('active')
+    }else {
+        document.querySelector('.header').classList.remove('active')
+    }
+}
+
+window.onload = () => {
+    if(window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+        }else {
+            document.querySelector('.header').classList.remove('active')
+        }
+    }
